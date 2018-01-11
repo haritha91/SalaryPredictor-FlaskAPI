452c3a8b7b6081057f964094048079b67332502c
Haritha Thilakarathne <harithalht@outlook.com>
2018-01-11 17:36:31 +0530

    Add project files.

---
+++ FlaskMLAPI_2/MLService.py
@@ -0,0 +1,25 @@
+#from flask import Flask
+#from urllib import request
+from json import load
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            years_of_experience = float(data["yearsOfExperience"])
+            
+            lin_reg = joblib.load("./linear_regression_model.pkl")
+        except ValueError:
+            return jsonify("Please enter a number.")
+
+        return jsonify(lin_reg.predict(years_of_experience).tolist())
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
+
